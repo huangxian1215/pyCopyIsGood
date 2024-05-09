066763a991645022073f7430338d35d1b9dc0513
huangxian <hihuangxian@163.com>
2024-05-09 16:32:26 +0800

    shot cut copy txt

--- ToolWidget.py
+++ ToolWidget.py
@@ -1,15 +1,21 @@
 from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import QDialog, QToolButton, QApplication
+from PyQt5.QtWidgets import QDialog, QApplication, QVBoxLayout, QPushButton
 from widget import Widget
 
 class ToolWidget(QDialog):
     def __init__(self):
         super().__init__()
-        self.resize(200,50)
-        btn = QToolButton(self)
+        self.resize(250,50)
+        self.setWindowTitle('图像转文字--百度识别')
+
+        btn = QPushButton(self)
         btn.setText('开始')
-        btn.setGeometry(20,20,50,20)
+        # btn.setGeometry(20,20,50,20)
+        btn.setMinimumHeight(40)
         btn.clicked.connect(self.startCut)
+        vlay = QVBoxLayout(self)
+        vlay.addWidget(btn)
+        vlay.setContentsMargins(0,0,0,0)
     def startCut(self):
         print('startCut')
         self.hide()
--- widget.py
+++ widget.py
@@ -1,5 +1,6 @@
 from PyQt5.QtGui import QKeyEvent, QMouseEvent, QPaintEvent, QPixmap, QPainter, QPen
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QSpacerItem, QSizePolicy, \
+    QPlainTextEdit, QApplication
 from PyQt5.QtCore import Qt, QRect
 
 
@@ -10,6 +11,7 @@ class Widget(QWidget):
         self.showMaximized()
         self.setWindowFlag(Qt.FramelessWindowHint)
         self.initPos()
+        self.setCursor(Qt.CrossCursor)
 
     def initPos(self):
         self.x0 = self.y0 = self.x1 = self.y1 = 0
@@ -31,11 +33,50 @@ class Widget(QWidget):
         self.y0 = self.y1 = event.y()
 
     def mouseReleaseEvent(self, event: QMouseEvent) -> None:
+        self.setCursor(Qt.ArrowCursor)
         pix = self.grab(self.rect)
-        pix.save('./1.png')
+        pix.save('./tmp.png')
         self.initPos()
         self.close()
+        self.showCurrent(pix)
 
+    def showCurrent(self, pix:QPixmap):
+        self.label = QLabel()
+        self.label.setPixmap(pix)
+        self.widget = QWidget()
+        self.widget.setWindowTitle('图像转文字--百度识别')
+        vlay = QVBoxLayout(self.widget)
+        vlay.addWidget(self.label)
+        self.widget.show()
+        hlay = QHBoxLayout()
+        self.btnReg = QPushButton(self.widget)
+        self.btnReg.setText('识别')
+
+        horizontalSpacer = QSpacerItem(40, 17, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        hlay.addItem(horizontalSpacer)
+        hlay.addWidget(self.btnReg)
+        vlay.addLayout(hlay)
+        self.plaintext = QPlainTextEdit(self.widget)
+        vlay.addWidget(self.plaintext)
+
+        hlay1 = QHBoxLayout()
+        self.btnCopy = QPushButton(self.widget)
+        self.btnCopy.setText('复制')
+        horizontalSpacer1 = QSpacerItem(40, 17, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        hlay1.addItem(horizontalSpacer1)
+        hlay1.addWidget(self.btnCopy)
+        vlay.addLayout(hlay1)
+        vlay.setContentsMargins(0, 0, 0, 0)
+
+        self.btnReg.clicked.connect(self.baiduPic2txt)
+        self.btnCopy.clicked.connect(self.copyTxt)
+
+    def baiduPic2txt(self):
+        self.plaintext.setPlainText('asdf')
+
+    def copyTxt(self):
+        clipboard = QApplication.clipboard()
+        clipboard.setText(self.plaintext.toPlainText())
 
     def mouseMoveEvent(self, event: QMouseEvent) -> None:
         self.x1 = event.x()
